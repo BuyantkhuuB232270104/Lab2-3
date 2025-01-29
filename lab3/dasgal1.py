c899dca4881cf81c3dce8bbedee2cfc18c40d1df
BuyantkhuuB232270104 <cbuyantkhuu@gmail.com>
2025-01-29 13:35:03 +0800

    dasgal1 iig hiilee

---
+++ lab3/dasgal1.py
@@ -0,0 +1,12 @@
+# MADLIB 1
+course_id = input("Хичээлийн код: ")
+course_name = input("Хичээлийн нэр: ")
+credit = input("Хичээлийн кредит: ")
+madlib = "Энэ хичээлийн код нь {}".format(course_id).lower() + \
+    ", хичээлийн нэр нь {}".format(course_name).lower() + \
+    " бөгөөд уг хичээл нь {}".format(credit) + " кредитийн хичээл юм."
+madlib1 = "Энэ хичээлийн код нь {}".format(course_id).title() + \
+    ", хичээлийн нэр нь {}".format(course_name).title() + \
+    " бөгөөд уг хичээл нь {}".format(credit) + " кредитийн хичээл юм."
+print(madlib)
+print(madlib1)
